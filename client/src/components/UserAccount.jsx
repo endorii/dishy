c17596f09cf584df3b7162d0e9d131fb06b891d2
Serhii Propolov <89745760+endorii@users.noreply.github.com>
2023-09-24 15:34:29 +0300

    Filled out the account information page

--- client/src/components/UserAccount.jsx
+++ client/src/components/UserAccount.jsx
@@ -8,14 +8,18 @@ import { useNavigate } from 'react-router-dom';
 const UserAccount = () => {
 
     const dispatch = useDispatch();
+
     const navigate = useNavigate();
-    const { isAuth } = useSelector(state => state.user);
+    const { name, phone, email, company } = useSelector(state => state.user.user)
+    const token = localStorage.getItem("token");
 
     useEffect(() => {
-        if (isAuth === false) {
+        if (!token) {
             navigate('/auth/login')
         }
-    }, [isAuth])
+    }, [token])
+
+    
 
     return (
         <div className='flex flex-col' >
@@ -28,10 +32,30 @@ const UserAccount = () => {
             </div>
             <hr className='border-t-1 border-slate-300 my-10' />
 
-            <div>
-                {/* Add some information aboute account:
-                    - date of creation \t
-                    - account editing functionality */}
+            <div className='flex flex-col gap-16'>
+
+                <div className='flex flex-raw items-center gap-[20%]'>
+                    <div className='mr-2 w-32'>Ім'я акаунту: </div>
+                    <input type="text" value={name} className="pl-5 block w-96 h-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                </div>
+
+
+                <div className='flex flex-raw items-center gap-[20%]'>
+                    <div className='mr-2 w-32'>Назва компанії: </div>
+                    <input type="text" value={company} className="pl-5 block w-96 h-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                </div>
+
+
+                <div className='flex flex-raw items-center gap-[20%]'>
+                    <div className='mr-2 w-32'>Email: </div>
+                    <input type="text" value={email} className="pl-5 block w-96 h-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                </div>
+
+                <div className='flex flex-raw items-center gap-[20%]'>
+                    <div className='mr-2 w-32'>Контактний номер телефону власника: </div>
+                    <input type="text" value={"+" + phone} className="pl-5 block w-96 h-10 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                </div>
+
             </div>
 
         </div>
