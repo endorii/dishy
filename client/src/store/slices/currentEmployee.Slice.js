91b1bad2eb591de55f3388049f75e08db27bff9b
Serhii Propolov <89745760+endorii@users.noreply.github.com>
2023-10-09 11:47:38 +0300

    Current employee slice

--- client/src/store/slices/currentEmployee.Slice.js
+++ client/src/store/slices/currentEmployee.Slice.js
@@ -1,9 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getCurrentEmployee } from "../../components/employee";
 
 const initialState = {
     currentEmployee: {},
+    initialWorkingTime: '',
+    finalWorkingTime: '',
     isAuth: false
 }
+
+export const fetchCurrentEmployee = createAsyncThunk(
+    'currentEmployee/fetchCurrentEmployee',
+    async () => {
+        const response = getCurrentEmployee();
+        return response;
+    }
+)
 
 const currentEmployeeSlice = createSlice({
     name: "currentEmployee",
@@ -13,13 +24,33 @@ const currentEmployeeSlice = createSlice({
             state.isAuth = true;
             state.currentEmployee = action.payload;
         },
+        setInitialWorkingTime(state, action){
+            state.isAuth = true;
+            state.initialWorkingTime = action.payload;
+        },
         logout(state) {
             state.isAuth = false;
             state.currentEmployee = {};
         }
     },
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchCurrentEmployee.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        })
+        .addCase(fetchCurrentEmployee.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isAuth = true;
+            state.currentEmployee = action.payload;
+        })
+        .addCase(fetchCurrentEmployee.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        })
+    }
 })
 
-export const { setCurrentEmployee, logout } = currentEmployeeSlice.actions;
+export const { setCurrentEmployee, logout, setInitialWorkingTime } = currentEmployeeSlice.actions;
 
 export default currentEmployeeSlice.reducer;
