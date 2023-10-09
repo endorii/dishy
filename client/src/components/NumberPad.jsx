18811acbc447d7ba60da0cc12481d12b20f76f9f
Serhii Propolov <89745760+endorii@users.noreply.github.com>
2023-10-09 11:47:28 +0300

    Logic to display current employee

--- client/src/components/NumberPad.jsx
+++ client/src/components/NumberPad.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Delete from '../assets/icons/delete.svg';
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginCurrentEmployee } from "./employee";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Modal } from './Modal'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const NumberPad = () => {
 
+    const navigate = useNavigate();
 
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     const [inputText, setInputText] = useState('');
+
+    const [open, setOpen] = useState(false);
+
+    const { name } = useSelector(state => state.currentEmployee.currentEmployee)
 
     const dispatch = useDispatch()
 
@@ -25,8 +34,71 @@ const NumberPad = () => {
         setInputText('');
     }
 
+    const [now, setNow] = useState(new Date().toLocaleTimeString());
+
+
+    const notify = () => toast.info("Робочу зміну почато!", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "light"
+    });
+
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setNow(new Date().toLocaleTimeString());
+        }, 1000);
+        return () => clearInterval(timer);
+    }, []);
+
+
+
     return (
         <>
+            {open ?
+                <Modal>
+                    <div className='flex justify-center '>
+                        <div className='absolute bg-gray-200 shadow-xl w-1/3 h-auto z-10 rounded-md mt-16'>
+                            <div className='flex flex-col items-center mx-3 gap-4 mt-5'>
+                                <div className="font-thin text-xl">Вітаємо, {name}</div>
+                                <div className="font-thin text-xl">Розпочати робочу зміну?</div>
+                                <div className="font-thin text-xl">Початок зміни о: {now}</div>
+                                <div className="flex flex-row gap-x-5 mt-4">
+                                    <button className="flex items-center bg-red-500 hover:bg-red-600 rounded-lg mb-7 px-7 py-2 text-white font-medium drop-shadow-md" onClick={() => { setOpen(false) }}>Ні</button>
+                                    <div>
+                                        <button onClick={() => {
+                                            setOpen(false);
+                                            clearPad();
+                                            notify();
+                                            setTimeout(() => {
+                                                navigate('/service');
+                                            }, 3000);
+                                        }} className="flex items-center bg-green-500 hover:bg-green-600 rounded-lg mb-7 px-7 py-2 text-white font-medium drop-shadow-md">Так</button>
+
+                                    </div>
+                                </div>
+
+                                <div className="absolute top-1 right-1 cursor-pointer" onClick={() => { setOpen(false) }}>✖</div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal> : null}
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable={false}
+                pauseOnHover={false}
+                theme="light" />
             <div className="flex justify-center m-10 sm:mx-auto sm:w-full sm:max-w-sm ">
                 <input className="w-full h-20 bg-gray-400/10 rounded-lg text-center font-bold text-white text-6xl" type="password" value={inputText} onChange={(e) => setInputText(e.target.value)} />
             </div>
@@ -43,11 +115,11 @@ const NumberPad = () => {
                 <button onClick={() => handleClick(0)} className="w-1/2 text-center bg-gray-400/10 w-24 h-24 text-2xl hover:bg-gray-400/30">
                     0
                 </button>
-                <Link to="/">
-                    <button onClick={() => { clearPad(); dispatch(loginCurrentEmployee(inputText)) }} className="w-1/2 text-center bg-emerald-400/10 w-24 h-24 text-2xl hover:bg-emerald-400/30 font-medium">
-                        Go
-                    </button>
-                </Link>
+                <button onClick={() => {
+                    dispatch(loginCurrentEmployee(inputText)); setOpen(true)
+                }} disabled={inputText.length < 4} className="w-1/2 text-center bg-emerald-400/10 w-24 h-24 text-2xl hover:bg-emerald-400/30 font-medium disabled:opacity-25 disabled:hover:bg-emerald-400/10">
+                    Go
+                </button>
 
             </div>
         </>
--- client/src/components/Service.jsx
+++ client/src/components/Service.jsx
@@ -1,8 +1,21 @@
 import { Link, Outlet } from "react-router-dom"
 
 import Notification from '../assets/icons/notification.svg';
+import { useDispatch, useSelector } from "react-redux";
+import GreenDot from '../assets/img/green_dot.png'
+import RedDot from '../assets/img/red_dot.png'
+import { useEffect } from "react";
+import { fetchCurrentEmployee } from "../store/slices/currentEmployee.Slice";
+
+
 
 export const Service = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchCurrentEmployee());
+    }, [])
+    const {name} = useSelector(state => state.currentEmployee.currentEmployee);
+    const {isAuth} = useSelector(state => state.currentEmployee);
     return (
         <div className="flex flex-col h-screen w-screen text-white justify-center fixed">
             <aside className="">
@@ -10,18 +23,18 @@ export const Service = () => {
                     <div className="flex justify-between items-center h-full">
                         <ul className="flex h-full items-center">
                             <li>
-                                <Link to="orders" className="p-5 border border-x-zinc-800 border-y-0 text-center hover:bg-gray-600">Замовлення</Link>
+                                <Link to="orders" className="p-5  text-center hover:bg-gray-600">Замовлення</Link>
                             </li>
                             <li>
-                                <Link to="tables" className="p-5 border border-r-zinc-800 border-y-0 border-l-0 text-center hover:bg-gray-600">Столи</Link>
+                                <Link to="tables" className="p-5 text-center hover:bg-gray-600">Столи</Link>
                             </li>
                         </ul>
                         <div className="flex">
-                            <div className="p-4 border border-x-zinc-800 border-y-0 text-center hover:bg-gray-600">
+                            <div className="p-4  text-center hover:bg-gray-600">
                                 <img className="w-9" src={Notification} alt="" />
                             </div>
-                            <div className="p-5 border border-r-zinc-800 border-y-0 border-l-0 text-center hover:bg-gray-600">account</div>
-                            <div className="p-5 border border-r-zinc-800 border-y-0 border-l-0 border-r-0 text-center hover:bg-gray-600" >status</div>
+                            <div className="p-5 text-center hover:bg-gray-600 font-medium"><Link to="account">{name}</Link></div>
+                            <div className="p-5 border-r-0 text-center hover:bg-gray-600 flex items-center" >{isAuth ? <img className=" h-2" src={GreenDot} alt="" /> : <img className=" h-2" src={RedDot} alt="" />}</div>
                         </div>
                     </div>
                 </div>
