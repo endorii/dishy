2ab3ada15e002b1ba2bb15eed3ba191a145525b1
Serhii Propolov <89745760+endorii@users.noreply.github.com>
2023-10-12 22:02:16 +0300

    Minor changes and some improvements

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,7 +2,7 @@ import { createPortal } from 'react-dom';
 
 export const Modal = ({ children }) => {
     return createPortal(
-        <div className='modal flex justify-center w-full h-full absolute top-0 l-0 bg-gray-700/[0.6]'>
+        <div className='modal flex justify-center w-full h-full absolute top-0 l-0 bg-gray-700/[0.6] overflow-auto'>
             {children}
         </div>,
         document.getElementById('modal')
