0618b3cbcc98f0206d50475bf3457a91f2c126e9
Serhii Propolov <89745760+endorii@users.noreply.github.com>
2023-10-29 17:33:39 +0200

    added the functionality of adding guests and dishes

--- client/src/components/NewOrderModal.jsx
+++ client/src/components/NewOrderModal.jsx
@@ -15,13 +15,40 @@ import Close from '../assets/icons/close.svg'
 import Time from '../assets/icons/time.svg'
 import { useState } from "react";
 import { MenuItems } from "../functions";
+import { Modal } from './Modal';
+import { PayOrder } from './PayOrder';
 
-export const NewOrderModal = ({ setOpenNewOrderMenu, setOpenPayOrder }) => {
+export const NewOrderModal = ({ setOpenNewOrderMenu, setOpenPayOrder, openPayOrder }) => {
 
-    const [currentFood, setCurrentFood] = useState('');
+    const [guests, setGuests] = useState([]);
+    const [currentCategoryFood, setCurrentCategoryFood] = useState('');
+    const [currentGuest, setCurrentGuest] = useState({})
+
+    const addGuest = () => {
+        const newGuest = {
+            id: guests.length + 1,
+            order: [],
+        };
+
+        setGuests([...guests, newGuest]);
+    };
+
+    const selectFoodForGuest = (currentGuest, foodItem) => {
+        const updatedGuests = [...guests];
+        currentGuest.order.push(foodItem);
+        setGuests(updatedGuests);
+    };
+
+    const removeGuest = (guestId) => {
+        const updatedGuests = guests.filter((guest) => guest.id !== guestId);
+        setGuests(updatedGuests);
+    };
 
     return (
         <>
+            {openPayOrder ? <Modal>
+                <PayOrder setOpenPayOrder={setOpenPayOrder} />
+            </Modal> : null}
             <div className='flex justify-center'>
                 <div className=""><img className="absolute top-14 right-20 z-20 w-12 cursor-pointer" src={Close} alt="" onClick={() => {
                     setOpenNewOrderMenu(false)
@@ -29,24 +56,24 @@ export const NewOrderModal = ({ setOpenNewOrderMenu, setOpenPayOrder }) => {
                 <div className='absolute bg-white shadow-xl w-[95%] z-10 rounded-md mt-10 bg-gray-400'>
                     <div>
                         <div className="flex rounded-xl h-[90vh]">
-                            <div className="bg-gray-200 w-[30%] h-full flex flex-col justify-between">
+                            <div className="bg-gray-200 w-[30%] h-full flex flex-col justify-between overflow-x-auto">
                                 <div>
                                     <table className="w-full text-sm text-left text-gray-500 ">
                                         <thead className="text-xs text-gray-700 uppercase bg-gray-300">
                                             <tr>
-                                                <th scope="col" className="px-3 py-3 text-center">
+                                                <th scope="col" className="px-10 py-3">
                                                     Назва
                                                 </th>
-                                                <th scope="col" className="px-3 py-3 text-center">
+                                                <th scope="col" className="px-1 py-3">
                                                     Кількість
                                                 </th>
-                                                <th scope="col" className="px-3 py-3 text-center">
+                                                <th scope="col" className="px-1 py-3">
                                                     Ціна
                                                 </th>
-                                                <th scope="col" className="px-3 py-3 text-center">
+                                                <th scope="col" className="px-1 py-3">
                                                     Загалом
                                                 </th>
-                                                <th scope="col" className="px-3 py-3 text-center">
+                                                <th scope="col" className="px-1 py-3">
 
                                                 </th>
                                             </tr>
@@ -55,8 +82,43 @@ export const NewOrderModal = ({ setOpenNewOrderMenu, setOpenPayOrder }) => {
 
                                         </tbody>
                                     </table>
-                                    <div className="p-5">
-                                        <button className="text-lg font-medium" >+ Додати гостя</button>
+
+                                    <div className="p-5 flex flex-col gap-y-3 items-start">
+                                        {guests.map((guest, i) => {
+                                            return (
+                                                <div key={i} className='bg-white w-full rounded-lg p-2' onClick={() => setCurrentGuest(guest)}>
+                                                    <div className='flex justify-between px-4'>
+                                                        <div className="text-lg font-medium">Гість {guest.id}</div>
+                                                        <div className="text-lg font-medium" onClick={() => { removeGuest(guest.id) }}>Видалити</div>
+                                                    </div>
+                                                    <div className='w-full'>
+
+                                                        <tbody className='bg-gray-100 p-3 m-2 rounded-xl w-full'>
+                                                            {guest.order.map((dish, i) => {
+                                                                return (
+                                                                    <tr className="bg-white border-b border-gray-300 text-gray-700">
+                                                                        <td className="px-3 py-3">
+                                                                            {dish.name}
+                                                                        </td>
+                                                                        <td className="px-20 py-3 text-center">
+                                                                            1
+                                                                        </td>
+                                                                        <td className="px-3 py-3 text-center">
+                                                                            {dish.value}
+                                                                        </td>
+                                                                        <td className="px-14 py-3 text-center">
+                                                                            {dish.value}
+                                                                        </td>
+                                                                    </tr>
+                                                                )
+                                                            })}
+                                                        </tbody>
+
+                                                    </div>
+                                                </div>
+                                            )
+                                        })}
+                                        <button onClick={() => addGuest()} className="text-lg font-medium" >+ Додати гостя</button>
                                     </div>
                                 </div>
 
@@ -67,7 +129,7 @@ export const NewOrderModal = ({ setOpenNewOrderMenu, setOpenPayOrder }) => {
                                     </div>
                                     <div className="flex justify-between">
                                         <button className="bg-white p-3 border-2 text-blue-500 border-blue-200 text-2xl rounded-lg text-blue-600 font-medium w-[47%]">. . .</button>
-                                        <button onClick={() => {setOpenPayOrder(true)}} className="w-[47%] bg-green-500 p-3 rounded-lg text-white font-medium">Оплатити замовлення</button>
+                                        <button onClick={() => { setOpenPayOrder(true) }} className="w-[47%] bg-green-500 p-3 rounded-lg text-white font-medium">Оплатити замовлення</button>
                                     </div>
                                 </div>
                             </div>
@@ -75,13 +137,13 @@ export const NewOrderModal = ({ setOpenNewOrderMenu, setOpenPayOrder }) => {
                                 <div className="">
                                     <h2 className="text-lg font-medium mb-5">Доступні товари для замовлення</h2>
                                     <button onClick={() => {
-                                        setCurrentFood('')
+                                        setCurrentCategoryFood('')
                                     }} className="text-lg font-medium mb-10 bg-blue-100 px-4 py-1 rounded-xl hover:bg-blue-200">Назад</button>
                                 </div>
                                 <div className="">
                                     <ul className="flex flex-wrap gap-10">
-                                        {currentFood ? currentFood.items.map((item, i) => (
-                                            <li key={i} className="bg-gray-100 rounded-lg shadow-md">
+                                        {currentCategoryFood ? currentCategoryFood.items.map((item, i) => (
+                                            <li key={i} className="bg-gray-100 rounded-lg shadow-md cursor-pointer" onClick={() => { selectFoodForGuest(currentGuest, item) }}>
                                                 <img className="w-[260px] h-[200px] object-cover object-[50% 100%] rounded-t-lg" src={item.src} alt={item.name} />
                                                 <div className="font-thin text-lg px-4 py-2">{item.name}</div>
                                                 <div className="flex justify-between">
@@ -91,7 +153,7 @@ export const NewOrderModal = ({ setOpenNewOrderMenu, setOpenPayOrder }) => {
                                             </li>
                                         )) : MenuItems.map((item, i) => (
                                             <li key={i} onClick={() => {
-                                                setCurrentFood(item);
+                                                setCurrentCategoryFood(item);
 
                                             }} className="bg-gray-100 rounded-lg shadow-md cursor-pointer">
                                                 <img className="w-[260px] h-[200px] object-cover object-[50% 100%] rounded-t-lg" src={item.logo} alt={item.alt} />
